597f7576c7cde24059fa037fbe5432c09484ded7
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-04-19 12:53:51 -0600

    configs setup

--- mix.exs
+++ mix.exs
@@ -41,7 +41,10 @@ defmodule Huzzah.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6.4", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.18"}
     ]
   end
 
